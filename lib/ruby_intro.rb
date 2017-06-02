a21d59fc5b56083592b11f9c4e423b985f00f1aa
matt182 <mattspring0@gmail.com>
2017-06-02 20:04:08 +0000

    init

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -2,34 +2,60 @@
 
 # Part 1
 
-def sum arr
-  # YOUR CODE HERE
+def sum(arr)
+  arr.inject(0, :+)
 end
 
-def max_2_sum arr
+
+def max_2_sum(arr)
   # YOUR CODE HERE
+  arr.sort.last(2).inject(0, :+)
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  !!arr.combination(2).find { |x, y| x + y == n }
 end
 
 # Part 2
 
 def hello(name)
   # YOUR CODE HERE
+  return "Hello, #{name}"
 end
 
 def starts_with_consonant? s
   # YOUR CODE HERE
+
+  (s =~ /\A(?=[^aeiou])(?=[a-z])/i) != nil
 end
 
 def binary_multiple_of_4? s
   # YOUR CODE HERE
+  # 'dhsfgsdj'
+  # '0101' s ~= /[2-9a-z]/i
+  return false if s.empty?
+  if s =~ /[^01]/
+    return false
+  else
+    s.to_i(2) % 4 == 0
+  end
 end
 
 # Part 3
 
 class BookInStock
 # YOUR CODE HERE
+attr_accessor :isbn
+attr_accessor :price
+def initialize(isbn, price)
+  raise ArgumentError if isbn.empty? || price<=0
+  @isbn = isbn
+  @price = price
+end
+
+def price_as_string
+  formated_price = sprintf( "%0.2f", price)
+  return "$#{formated_price}"
+end
+
 end
